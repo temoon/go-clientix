1b3177ae9c3ebc9f42e6adf9ef40c0c84878934f
Artem Novikov <novikov_a@corp.vk.com>
2022-03-28 00:38:44 +0300

    Phone only numbers

--- types/phone.go
+++ types/phone.go
@@ -11,30 +11,32 @@ func (t *Phone) UnmarshalJSON(b []byte) (err error) {
 		return
 	}
 
-	cleanedPhone := strings.Builder{}
-	cleanedPhone.WriteByte('+')
-
-	for i := 1; i < len(b)-1; i++ {
-		if b[i] >= '0' && b[i] <= '9' {
-			cleanedPhone.WriteByte(b[i])
-		}
-	}
-
-	*t = Phone(cleanedPhone.String())
+	*t = ParsePhone(string(b[1 : len(b)-1]))
 
 	return
 }
 
 func (t Phone) IsValid() bool {
-	if len(t) < 2 || t[0] != '+' {
+	if len(t) == 0 {
 		return false
 	}
 
-	for _, c := range t[1:] {
-		if c < '0' || c > '9' {
+	for i := 0; i < len(t); i++ {
+		if t[i] < '0' || t[i] > '9' {
 			return false
 		}
 	}
 
 	return true
+}
+
+func ParsePhone(data string) Phone {
+	builder := strings.Builder{}
+	for i := 0; i < len(data); i++ {
+		if data[i] >= '0' && data[i] <= '9' {
+			builder.WriteByte(data[i])
+		}
+	}
+
+	return Phone(builder.String())
 }
